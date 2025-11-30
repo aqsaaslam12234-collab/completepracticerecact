13a4d923d85216806da9bc881fe0a46bb23847b6
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-11-30 19:56:36 +0000

    save

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+
+import TestingComponent from './components/TestingComponent'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+
+     
+    <TestingComponent  />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TestingComponent.tsx
@@ -0,0 +1,9 @@
+// interface TestingComponentProps {
+// name:string;
+// }
+function TestingComponent (): void{
+    const displayName="Aqsa Aslam";
+    return
+    <h1>Name: {displayName}</h1>
+}
+export default TestingComponent
